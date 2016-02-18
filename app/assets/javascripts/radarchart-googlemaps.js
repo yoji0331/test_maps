3c60fd73839fd6b38d7652393923ecf9b69d8075
yoji0331 <smhsc251@yahoo.co.jp>
2016-02-18 11:07:13 +0900

    first commit

--- app/assets/javascripts/radarchart-googlemaps.js
+++ app/assets/javascripts/radarchart-googlemaps.js
@@ -4,6 +4,12 @@
 
 var RADAR_CHART = {};
 
+var formatData = [];
+for(var i=0;i<4;i++){
+    formatData[i] = new Array(6);
+}
+    // cnsole.log(formatData);
+
 RADAR_CHART.radarChart = function (index, scores) {
     'use strict';
 
@@ -30,7 +36,19 @@ RADAR_CHART.radarChart = function (index, scores) {
         .attr('width', w)
         .attr('height', h);
 
+    console.log(scores.length);
     dataset = [scores];
+
+    var k, axis =[], dataAxis;
+    for (k=0;k < scores.length;k++){
+        axis.push(0);
+        axis.push(max);        
+    }
+    dataAxis = [axis];
+    
+
+
+
 
     paramCount = dataset[0].length;
 
@@ -55,14 +73,14 @@ RADAR_CHART.radarChart = function (index, scores) {
     };
     grid();
 
-    label = function () {
+
+    label  = (function(){
         var result = [];
-        for (i = 0; i < paramCount; i += 1) {
-            result.push(max + 1);
+        for(var i=0; i<paramCount; i++){
+          result.push(max + 1);
         }
         return result;
-    };
-    label();
+      })();
 
     line = d3.svg.line()
         .x(function (d, i) {
@@ -84,9 +102,21 @@ RADAR_CHART.radarChart = function (index, scores) {
             return d3.scale.category10().range()[i];
         })
         .attr("stroke-width", 2)
+        .attr('fill', '#1f77b4');
+
+    svg.selectAll('path.axis')
+        .data(dataAxis)
+        .enter()
+        .append('path.axis')
+        .attr('d', function (d, i) {
+            return line(d) + "z";
+        })
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
         .attr('fill', 'none');
 
-    svg.selectAll("path.grid")
+
+    /*svg.selectAll("path.grid")
         .data(grid)
         .enter()
         .append("path")
@@ -95,24 +125,18 @@ RADAR_CHART.radarChart = function (index, scores) {
         })
         .attr("stroke", "black")
         .attr("stroke-dasharray", "2")
-        .attr('fill', 'none');
+        .attr('fill', 'none');*/
 
-    svg.selectAll("text")
-        .data(label)
-        .enter()
-        .append('text')
-        .text(function (i) {
-            return i - 1;
-        })
-        .attr("text-anchor", "middle")
-        .attr("dominant-baseline", "middle")
-        .attr('x', function (d, i) {
-            return rScale(d) * Math.cos(2 * Math.PI / paramCount * i - (Math.PI / 2)) + w / 2;
-        })
-        .attr('y', function (d, i) {
-            return rScale(d) * Math.sin(2 * Math.PI / paramCount * i - (Math.PI / 2)) + w / 2;
-        })
-        .attr("font-size", "15px");
+     svg.selectAll("text")
+     .data(label)
+     .enter()
+     .append('text')
+     .text(function(d, i){ return i+1; })
+     .attr("text-anchor", "middle")
+     .attr("dominant-baseline", "middle")
+     .attr('x', function(d, i){ return rScale(d) * Math.cos(2 * Math.PI / paramCount * i - (Math.PI / 2)) + w/2; })
+     .attr('y', function(d, i){ return rScale(d) * Math.sin(2 * Math.PI / paramCount * i - (Math.PI / 2)) + w/2; })
+     .attr("font-size", "15px");
 };
 
 RADAR_CHART.createMarker = function (spot, map) {
@@ -139,7 +163,7 @@ RADAR_CHART.attachInfoWindow = function (marker, name, blankScores, index) {
             infoWindow.open(marker.getMap(), marker);
             google.maps.event.addListener(infoWindow,'closeclick',function(){
                 infoWindow = null;
-                console.log(infoWindow);
+                // console.log(infoWindow);
             });
         }
 
@@ -147,6 +171,8 @@ RADAR_CHART.attachInfoWindow = function (marker, name, blankScores, index) {
                 RADAR_CHART.radarChart(index, blankScores);
             });
     });
+    console.log(blankScores.length);
+    
     
 };
 
@@ -159,6 +185,7 @@ $(document).ready(function () {
         options,
         map,
         markers = [];
+
 
     center = new google.maps.LatLng(40.691153, 141.089492);
 
@@ -186,7 +213,7 @@ $(document).ready(function () {
 
     $.getJSON("notes.json", function (spots) {
         
-        /*
+        
         // for(.. in ,,) 
         // dataが[object object]となり
         // 値が取得できない 
@@ -196,28 +223,38 @@ $(document).ready(function () {
         };
         for(i=0; i<spots.length;i++){
             var obj = spots[i];
-            console.log(obj);
-            var key;
-            for(key in obj){
-                console.log("---");
-                console.log("key:" + key);
-                if (!isObject(obj[key])){
-                    console.log("data:" + obj[key]);
+            // console.log(obj);
+            var key01;
+            var dataArray;
+            var j;
+            j=0;
+            for(key01 in obj){
+ // console.log("---");
+                 // console.log("key01:" + key01);
+                if (!isObject(obj[key01])){
+                   // console.log("data:" + obj[key01]);
                 } else {
-                    var tmp;
-                    for(tmp in obj[key]){
-                        console.log("tmp:" + tmp);
-                        console.log(obj[key][tmp]);
+                    var key02;
+                    dataArray = [];
+                    for( key02 in obj[key01]){
+                        // console.log("key02:" + key02);
+                     // console.log(obj[key01][key02]);
+                        dataArray.push(obj[key01][key02]);
                     }
+                   // console.log("i:" + i + ",j:" + j);
+                   // console.log(dataArray);
+                    formatData[i][j] = dataArray;
+                     // console.log("aaa"+formatData[0][j]);
+                    j++;
                 }
-
             }
-        }*/
+        }
         
 
         // 連想配列のキーを配列に入れる
         // 配列は作成できたが
         // マーカーが作成できない
+        /*
         var i;
         var dataAll;
         for (i = 0; i < spots.length; i += 1) {
@@ -225,17 +262,175 @@ $(document).ready(function () {
             dataAll.push(spots[i]["自然のすがた"]["水の流れはゆたかですか？"]);
             dataAll.push(spots[i]["自然のすがた"]["岸のようすは自然らしいですか？"]);
             dataAll.push(spots[i]["自然のすがた"]["魚が川をさかのぼれるだろうか？"]);
-           // console.log(dataAll);
+            // console.log(dataAll);
             blankScores = dataAll;
             markers[i] = RADAR_CHART.createMarker(spots[i], map);
             RADAR_CHART.attachInfoWindow(markers[i], spots[i]["name"], blankScores, i);
             // console.log(spots[i]["name"]);
         }
+        */
 
     });
 });
 
+
+// 自然のすがた
+// formatData[][0]
+$("#a0").on('click', function(e) {
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][0];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: 自然のすがた</p><p>1:岸のようすは自然らしいですか？</p><p>2:水の流れはゆたかですか？</p><p>3:魚が川をさかのぼれるだろうか？</p>';
+ 
+    e.preventDefault();
+});
+
+// ゆたかな生き物
+// formatData[][1]
 $("#a1").on('click', function(e) {
-    alert("a1-alert");
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][1];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: ゆたかな生き物</p><p>1:海底に生き物がいますか？</p><p>2:河原と水辺に植物が生えていますか？</p><p>3:魚がいますか？</p><p>4:鳥はいますか？</p>';
+
+    e.preventDefault();
+});
+
+// 水のきれいさ
+// formatData[][2]
+$("#a2").on('click', function(e) {
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][2];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: 水のきれいさ</p><p>1:水はきれいですか？</p><p>2:水はくさくないですか？</p><p>3:水は透明ですか？</p>';
+
+    e.preventDefault();
+});
+
+// 快適な水辺
+// formatData[][3]
+$("#a3").on('click', function(e) {
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][3];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: 快適な水辺</p><p>1:ごみが目につきますか？</p><p>2:どんなにおいを感じますか？</p><p>3:どんな音が聞こえますか？</p><p>4:川やまわりの景色は美しいですか？</p><p>5:水にふれてみたいですか？</p>';
+
+    e.preventDefault();
+});
+
+// 地域とのつながり
+// formatData[][4]
+$("#a4").on('click', function(e) {
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][4];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: 地域とのつながり</p><p>1:多くの人が利用していますか？</p><p>2:川にまつわる話を聞いたことがありますか？</p><p>3:水辺に近づきやすいですか？</p><p>4:環境の活動</p><p>5:産業などの活動</p>';
+
+    e.preventDefault();
+});
+
+// 総合平均
+// formatData[][5]
+$("#a5").on('click', function(e) {
+    var blankScores =[];
+    markers =[];
+    center = new google.maps.LatLng(40.691153, 141.089492);
+
+    options = {
+        zoom: 13,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map($('#map').get(0), options);
+
+    $.getJSON("notes.json", function(spots) {
+        for (var i=0;i<spots.length; i++){
+            blankScores = formatData[i][5];
+            markers[i] = RADAR_CHART.createMarker(spots[i], map);
+            RADAR_CHART.attachInfoWindow(markers[i],spots[i]["name"], blankScores, i);
+        }
+    });
+    document.getElementById('content').innerHTML = '<p>項目: 総合平均</p><p>1:ゆたかな生き物</p><p>2:地域とのつながり</p><p>3:快適な水辺</p><p>4:水のきれいさ</p><p>5:自然のすがた</p>';
     e.preventDefault();
 });
